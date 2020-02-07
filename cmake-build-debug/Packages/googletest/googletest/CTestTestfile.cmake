41a0c656f364024a1340adcd175d715820626238
CERZAR <stavropoltsev.t@gmail.com>
2020-02-07 10:45:15 +0300

    DFD

---
+++ cmake-build-debug/Packages/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cezar/CLionProjects/RPG_GAME_lin/Packages/googletest/googletest
+# Build directory: /home/cezar/CLionProjects/RPG_GAME_lin/cmake-build-debug/Packages/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
