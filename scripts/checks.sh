0cf537cbad5991a98971113df95558b0abf463c6
CERZAR <stavropoltsev.t@gmail.com>
2020-02-11 01:10:07 +0300

    fix checks

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -3,8 +3,7 @@ set -e
 
 # shellcheck disable=SC2006
 files=`find . -name "*.cpp" -or -name ".h"`
-filter=-build/c++11
-#,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
+filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
 echo "$files" | xargs cpplint --filter=$filter
 export CTEST_OUTPUT_ON_FAILURE=true
 
